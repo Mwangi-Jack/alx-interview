6c3003fbb6b917ff77aa2c704d9655d269d60391
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-04 18:08:01 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 """lockboxes"""
 
-def canUnlockAll(boxes):
-    """lockboxes"""
+from typing import List
+
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """
+    This function takes in a list 'boxes' which contains lists of
+    integer values
+    """
+    def dfs(box: int, unlocked: set) -> None:
+        if box in unlocked:
+            return
+
+        unlocked.add(box)
+
+        for key in boxes[box]:
+            if key not in unlocked and key < len(boxes):
+                dfs(key, unlocked)
+
     unlocked = set()
-    stack = [0]
 
-    while stack:
-        current_box = stack.pop()
-
-        if current_box not in unlocked:
-            unlocked.add(current_box)
-
-            for key in boxes[current_box]:
-                if key not in unlocked:
-                    stack.append(key)
+    dfs(0, unlocked)
 
     return len(unlocked) == len(boxes)
