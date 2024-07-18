8cecaa94451cf3d065e6bc009894917b73fa083e
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-18 15:04:27 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,9 +7,19 @@ import signal
 
 
 LINE_COUNT = 0
-STATUS_CODE_DICT = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+STATUS_CODE_DICT = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 TOTAL_FILE_SIZE = 0
-REGEX_PATTERN = r'(?P<ip>[\d.]+) - \[(?P<date>[^\]]+)\] "(?P<method>\w+) (?P<url>[^ ]+) (?P<protocol>[^"]+)" (?P<status>\d+) (?P<size>\d+)'
+REGEX_PATTERN = (
+    r'(?P<ip>[\d.]+) - \['
+    r'(?P<date>[^\]]+)\] '
+    r'"(?P<method>\w+) '
+    r'(?P<url>[^ ]+) '
+    r'(?P<protocol>[^"]+)" '
+    r'(?P<status>\d+) '
+    r'(?P<size>\d+)'
+)
+
 
 def interrupt_handler(sig, frame):
     """keyboardInterrupt handler"""
@@ -19,10 +29,11 @@ def interrupt_handler(sig, frame):
 
     sys.exit()
 
+
 for line in sys.stdin:
     LINE_COUNT += 1
 
-    if LINE_COUNT % 11 == 0 :
+    if LINE_COUNT % 11 == 0:
         print(f'File size: {TOTAL_FILE_SIZE}')
         for key, value in STATUS_CODE_DICT.items():
             print(f'{key}: {value}')
@@ -39,4 +50,3 @@ for line in sys.stdin:
             STATUS_CODE_DICT[status] += 1
 
     signal.signal(signal.SIGINT, interrupt_handler)
-
