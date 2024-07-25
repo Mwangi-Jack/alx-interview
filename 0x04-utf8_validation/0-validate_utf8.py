56a054e41ab0da8bd7e6bfda2dca40a067126b2f
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-25 13:29:04 +0300

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,20 +4,7 @@
 from typing import List
 
 
-# def validUTF8(data: List[int]) -> bool:
-#     """
-#     This method takes in a list of integer values 'data'
-#     and validates if its a UFT8 encoded  or not returning a
-#     boolean
-#     """
-#     for enc in data:
-#         if enc >= 255:
-#             return False
-
-#     return True
-
-
-def validUTF8(data):
+def validUTF8(data: List[int]) -> bool:
     """
     This method takes in a list of integer values 'data'
     and validates if its a UFT8 encoded  or not returning a
