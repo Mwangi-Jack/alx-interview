6543ec4d37eedbc3d85a67937dc4aba104e7ced8
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-28 17:41:34 +0300

    sieve algorithm revision

---
+++ 0x02-minimum_operations/1-prime_nums.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Get prime numbers using sieve algorithim"""
+
+from typing import List
+
+from numpy import False_
+
+
+def get_prime_nums(n: int) -> List[int]:
+    """
+    This function takes in an integer 'n' and returns
+    a list of prime numbers from 0 to 'n'
+    """
+    tt = [True]* (n + 1)
+
+    a = 2
+
+    while a * a <= n:
+        if tt[a]:
+            for i in range(a *  a, n + 1, a):
+                tt[i] = False
+        a += 1
+
+    print([i for i in range(2, n) if tt[i]])
+    print(tt)
+    return [i for i in range(2, n + 1) if tt[i]]
+
+
+get_prime_nums(10)
