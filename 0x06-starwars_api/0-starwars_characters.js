53610181e0606671d1758f418b2de7ba1b0657fa
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-16 14:28:42 +0300

    created a script to fecth movie characters  from star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,20 +6,16 @@ const movieID = process.argv[2];
 const baseUrl = 'https://swapi-api.alx-tools.com/api';
 
 async function getMovieCharacters () {
-  if (movieID) {
-    const response = await axios.get(`${baseUrl}/films/${movieID}`);
-    const movieData = response.data;
+  const response = await axios.get(`${baseUrl}/films/${movieID}`);
+  const movieData = response.data;
 
-    const movieCharacters = movieData.characters;
+  const movieCharacters = movieData.characters;
 
-    for (const url of movieCharacters) {
-      const resp = await axios.get(url);
-      const characterData = resp.data;
+  for (const url of movieCharacters) {
+    const resp = await axios.get(url);
+    const characterData = resp.data;
 
-      console.log(characterData.name);
-    }
-  } else {
-    console.log('Pleas provide Movie ID');
+    console.log(characterData.name);
   }
 }
 
