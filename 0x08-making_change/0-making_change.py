26e2053f9714fe6d193966130fcf358afd9a26c2
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-23 16:06:57 +0300

    making change problem using greedy algorithm

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Make Change Problem"""
+from typing import List
+
+def makeChange(coins: List[int], total: int) -> int:
+    """
+    this method takes two arguments 'coins'-
+    integer array and 'total'- integer and determines
+    the fewest number of coins needed to meet a given
+    amount 'total'
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    count = 0
+
+    for coin in coins:
+        if total == 0:
+            break
+
+        count += total // coin
+        total %= coin
+
+    return count if total == 0 else -1
+
